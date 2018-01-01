691d103035de139796fd3c932bff9bb22f0524b8
Sandy Vanderbleek <sandy.vanderbleek@gmail.com>
2017-12-31 20:30:23 -0500

    fail proof

--- Ch2.thy
+++ Ch2.thy
@@ -2,11 +2,22 @@ theory Ch2
 imports Main
 begin
   fun add :: "nat \<Rightarrow> nat \<Rightarrow> nat" where
-    "add 0 n = n" |
-    "add (Suc m) n = Suc (add m n)"
+    "add 0 n = n"
+  | "add (Suc m) n = Suc (add m n)"
 
-  lemma add02: "add m 0 = m"
+  lemma add_zero: "add m 0 = m"
     apply(induction m)
+    apply(auto)
+  done
+
+  lemma add_succ: "add (Suc m) n = add m (Suc n)"
+    apply(induction m)
+    apply(auto)
+  done
+
+  lemma add_comm: "add m n = add n m"
+    apply(induction m)
+    apply(simp add: add_zero[symmetric])
     apply(auto)
   done
 end
